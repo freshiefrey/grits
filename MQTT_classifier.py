45cba380511943b2f99dd67c680fe770097e68b7
tomyummmm <mervin989@gmail.com>
2020-11-07 01:14:39 +0800

    MQTT Classifier and broker for gestures

---
+++ MQTT_classifier.py
@@ -0,0 +1,71 @@
+import paho.mqtt.client as mqtt
+import numpy as np
+import json
+
+from keras.models import load_model
+import tensorflow as tf
+from tensorflow.python.keras.backend import set_session
+import numpy as np
+
+MODEL_NAME = '../../lstm_models/lstm_model_100_50_96%'
+
+URL = "127.0.0.1" #change to cloud IP
+SENSOR = "GRITS/Sensor"
+MOTION = "GRITS/Motion_Data"
+GESTURE = "GRITS/Gesture"
+classes = ["Buddha clap", "Knob", "Pushback", "Swipe"]
+
+
+session = tf.compat.v1.Session(graph=tf.compat.v1.Graph())
+
+with session.graph.as_default():
+	set_session(session)
+	model = load_model(MODEL_NAME)
+
+
+def on_connect(client, userdata, flags, rc):
+	if rc == 0:
+		print("Successfully connected to broker.")
+		client.subscribe(MOTION)
+
+	else:
+		print("Connection failed with code: %d." % rc)
+
+def classify_gesture(data):
+	print("Start classifying")
+	with session.graph.as_default():
+		set_session(session)
+		prediction = model.predict(data)
+		index = np.argmax(result)
+	print("Done.")
+	return classes[index]
+
+def on_message(client, userdata, msg):
+	# Payload is in msg. We convert it back to a np 3D array
+	new = json.loads(msg.payload)
+
+	# Recreate the data
+	data = np.array(new)
+	print(data)
+	result = classify_gesture(data)
+
+
+	print("Sending results: ", result)
+	client.publish(GESTURE, json.dumps(result))
+
+def setup(hostname):
+	client = mqtt.Client()
+	client.on_connect = on_connect
+	client.on_message = on_message
+	client.connect(hostname)
+	client.loop_start()
+	return client
+
+def main():
+	setup(URL)
+
+	while True:
+		pass
+
+if __name__ == '__main__':
+	main()
---
+++ MQTT_gesture.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+import json
+
+URL = "127.0.0.1" #change to cloud IP
+SENSOR = "GRITS/Sensor"
+MOTION = "GRITS/Motion_Data"
+GESTURE = "GRITS/Gesture"
+
+def on_connect(client, userdata, flags, rc):
+	print("Connected with result code " + str(rc))
+	client.subscribe(GESTURE)
+
+def on_message(client, userdata, msg):
+	result = json.loads(msg.payload)
+	print("Prediction: " + result)
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+print("Connecting")
+client.connect(URL, 1883, 60)
+client.loop_forever()
